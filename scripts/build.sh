063cedf56dab46f8802598000e39a9aa9c3fe9c0
Kenneth Endfinger <kaendfinger@gmail.com>
2017-06-24 14:02:59 -0400

    Add pacman.conf and package list.

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,9 +2,12 @@
 set -e
 
 cd "$(dirname ${0})/.."
+
+PKGS=($(cat configs/packages | grep -v -e "^#" -e "^$" | tr '\n' ' '))
+
 [[ -d build/rootfs ]] && echo "Cleaning up after last build..." && sudo rm -rf build/rootfs
 mkdir -p build/rootfs
-sudo pacstrap -d build/rootfs base base-devel
+sudo pacstrap -C configs/pacman.conf -d build/rootfs "${PKGS[@]}"
 sudo cp -R overlay/* build/rootfs
 sudo arch-chroot build/rootfs /cinit.sh
 cd build/rootfs
--- scripts/import-master.sh
+++ scripts/import-master.sh
@@ -5,4 +5,4 @@ readonly MASTER="EB42FFEC"
 readonly GPG_CMD="sudo gpg --no-default-keyring --keyring /etc/systemd/import-pubring.gpg --keyserver hkp://pgp.mit.edu"
 
 ${GPG_CMD} --recv-keys "${MASTER}"
-echo -e "trust\n5\ny\n" | ${GPG_CMD} --command-fd 0 --edit-key "${MASTER}" > /dev/null 2>&1
+echo -e "5\n" | ${GPG_CMD} --command-fd 0 --edit-key "${MASTER}" trust quit > /dev/null 2>&1

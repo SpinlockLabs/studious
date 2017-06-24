19024cefc8f573ded3aaf616eaceb02ddc0a0c1a
Kenneth Endfinger <kaendfinger@gmail.com>
2017-06-23 23:01:08 -0400

    Ensure the GPG key is trusted.

--- scripts/import-master.sh
+++ scripts/import-master.sh
@@ -2,6 +2,7 @@
 set -e
 
 readonly MASTER="EB42FFEC"
+readonly GPG_CMD="sudo gpg --no-default-keyring --keyring /etc/systemd/import-pubring.gpg --keyserver hkp://pgp.mit.edu"
 
-sudo gpg --no-default-keyring --keyring /etc/systemd/import-pubring.gpg --keyserver hkp://pgp.mit.edu --recv-keys "${MASTER}"
-
+${GPG_CMD} --recv-keys "${MASTER}"
+echo -e "trust\n5\ny\n" | ${GPG_CMD} --command-fd 0 --edit-key "${MASTER}" > /dev/null 2>&1
